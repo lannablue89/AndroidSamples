aac769a609595b1abecdbba772933d2f27049d6e
lanna <lanna.blue89@gmail.com>
2017-01-19 00:43:52 +0700

    add test case and some first CI configs

--- AndroidSamples/app/src/main/java/com/lanna/android/samples/MainActivity.java
+++ AndroidSamples/app/src/main/java/com/lanna/android/samples/MainActivity.java
@@ -1,7 +1,10 @@
 package com.lanna.android.samples;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final TextView tvHi = (TextView) findViewById(R.id.tvHi);
+        final EditText etInput = (EditText) findViewById(R.id.etInput);
+        findViewById(R.id.btnClickMe).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                tvHi.setVisibility(tvHi.getVisibility() == View.VISIBLE ? View.GONE : View.VISIBLE);
+                tvHi.setText(etInput.getText());
+            }
+        });
     }
 }
